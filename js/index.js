b5263063de32217cf3df334f46d1c99f0220d16c
Barkha Bhojak <Barkha Bhojak>
2016-10-19 20:50:17 +0530

    Update 1

---
+++ js/index.js
@@ -0,0 +1,42 @@
+function TodoCtrl($scope) {
+  $scope.place1 = 'I need to..';
+
+  $scope.todos = [
+    {text:'Task 1', done:false},
+    {text: 'Task 2', done:false}
+  ];
+
+  $scope.getTotalTodos = function () {
+    return $scope.todos.length;
+  };
+
+
+  $scope.addTodo = function () {
+    if ($scope.formTodoText===""){
+      $scope.place1 = 'Please enter something';
+    }
+    else {
+      $scope.place1 = 'I need to..';
+      $scope.todos.push({text:$scope.formTodoText, done:false});
+      $scope.formTodoText = '';
+    }
+  };
+
+    $scope.clearCompleted = function () {
+        console.log("hello")
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
+        });
+    };
+}
+
+function Rooms($scope){
+  $scope.rooms = [
+    {text:'Room 1'},
+    {text: 'Room 2'},
+    {text:'Room 1'},
+    {text: 'Room 2'},
+    {text:'Room 1'},
+    {text: 'Room 2'}
+  ];
+}
