2db4048c0f59900c7c1e20ce67b78f1cad52c5fe
Barkha Bhojak <Barkha Bhojak>
2016-10-20 14:25:49 +0530

    Update 6

--- controllers/MainCtrl.js
+++ controllers/MainCtrl.js
@@ -20,7 +20,21 @@ myPage.config(function($stateProvider, $urlRouterProvider) {
 
 });
 
-myPage.controller('RoomsCtrl', function ($scope) {
+
+myPage.service("serviceID", function () {
+var _id = {};
+return {
+    getID: function () {
+        return _id;
+    },
+    setID: function (value) {
+         _id = value;
+     }
+};
+});
+
+
+myPage.controller('RoomsCtrl', function ($scope,serviceID) {
   $scope.rooms = [
     {text:'Room', id:'1'},
     {text:'Room', id:'2'},
@@ -34,6 +48,11 @@ myPage.controller('RoomsCtrl', function ($scope) {
   $scope.addRoom = function () {
     $scope.rooms.push({text:'Room',id:this.getTotalRooms()+1});
   }
+
+  $scope.checkRoom = function (value) {
+    serviceID.setID(value);
+  }
+
 });
 
 
@@ -71,11 +90,19 @@ myPage.controller('RoomsCtrl', function ($scope) {
 //     };
 // });
 
- myPage.controller('TodoCtrl', function ($scope) {
-   $scope.place1 = 'I need to ...';
+ myPage.controller('TodoCtrl', function ($scope,serviceID) {
+  $scope.place1 = 'I need to ...';
 	$scope.saved = localStorage.getItem('todos');
-	$scope.todos = (localStorage.getItem('todos')!==null) ? JSON.parse($scope.saved) : [ {text: 'Task 1', done: false}, {text: 'Task 2', done: false} ];
+	$scope.todos = (localStorage.getItem('todos')!==null) ? JSON.parse($scope.saved) : [ {text: 'Task 1', done: false, id: '1'},
+                                                                                        {text: 'Task 1', done: false, id: '2'},
+                                                                                        {text: 'Task 2', done: false, id:'3'} ];
 	localStorage.setItem('todos', JSON.stringify($scope.todos));
+  var id_room = serviceID.getID();
+
+  $scope.getIDRoom = function () {
+    console.log(id_room);
+    return id_room;
+  };
 
 	$scope.addTodo = function() {
     if ($scope.formTodoText===""){
@@ -84,7 +111,8 @@ myPage.controller('RoomsCtrl', function ($scope) {
 		else {
         $scope.todos.push({
   			text: $scope.formTodoText,
-  			done: false
+  			done: false,
+        id: id_room
   		});
   		$scope.formTodoText = ''; //clear the input after adding
   		localStorage.setItem('todos', JSON.stringify($scope.todos));
@@ -108,4 +136,17 @@ myPage.controller('RoomsCtrl', function ($scope) {
 		});
 		localStorage.setItem('todos', JSON.stringify($scope.todos));
 	};
+
+  $scope.$watch(function () { return serviceID.getID(); }, function (newValue, oldValue) {
+        if (newValue != null) {
+            $scope.id= newValue;
+            return newValue;
+        }
+    }, true);
+
+
+    $scope.checkValue = function(value) {
+      console.log(id_room==2);
+      return id_room == 2;
+    };
 });
